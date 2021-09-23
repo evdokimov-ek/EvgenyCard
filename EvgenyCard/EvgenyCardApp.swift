4e8d08d87050990d24c7e70970cd17a5715d8398
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-09-23 22:12:00 +0300

    Initial Commit

---
+++ EvgenyCard/EvgenyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvgenyCardApp.swift
+//  EvgenyCard
+//
+//  Created by 19336088 on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct EvgenyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
