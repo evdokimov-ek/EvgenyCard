920a2465926a102f96ba28fe69ff7df861eecea6
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-09-24 22:13:13 +0300

    finished 1st version

--- EvgenyCard/ContentView.swift
+++ EvgenyCard/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Image("IMG_20210912_110026")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Evgeniy Evdokimov").font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold().foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+7-123-456-6789", imageName: "phone.fill")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(width: 300, height: 50)
+            .foregroundColor(Color.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text).foregroundColor(.blue)
+            })
+            .padding(.all)
     }
 }
